dea963fc59b71c6b2f402cbf03d457cd5d51b5f9
Астанин Анатолий <tvelvepain@yandex.ru>
2024-03-11 01:32:54 +0700

    Lab2 Commit
    Added lab2 solution
    Added .pdf of tasks of lab1 and lab2

--- 2/9/9.py
+++ 2/9/9.py
@@ -1,31 +1,41 @@
+import argparse
 
 
 def format_text_block(frame_height, frame_width, file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
 
-    file = open(file_name, "r")
-    text = file.read()
-    file.close()
+        formatted_lines = []
+        for line in lines:
+            formatted_line = ''
+            line = line.strip()
+            while len(line) > frame_width:
+                formatted_line += line[:frame_width] + '\n'
+                line = line[frame_width:]
 
-    newText = []
-    globalLinesCursor = 0
+            formatted_line += line
+            formatted_lines.append(formatted_line)
 
-    lines = text.split("\n")
+        if len(formatted_lines) > frame_height:
+            formatted_lines = formatted_lines[:frame_height]
 
+        return '\n'.join(formatted_lines)
 
-    for line in lines:
-
-        symWritten = 0
-        heightCursor = 0
-        lineCursor = 0
-
-        while symWritten != len(line):
+    except Exception as e:
+        return str(e)
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Format text file and display its content within specified frame')
+    parser.add_argument('--frame-height', type=int, help='height of the frame in characters')
+    parser.add_argument('--frame-width', type=int, help='width of the frame in characters')
+    parser.add_argument('file_name', type=str, help='name of the input file')
+    args = parser.parse_args()
+
+    formatted_text = format_text_block(args.frame_height, args.frame_width, args.file_name)
+    print(formatted_text)
 
 
-            currentLineText = line[lineCursor:widthCursor]
-
-            newText.insert(globalLinesCursor+heightCursor, currentLineText)
-
-            lineCursor = widthCursor
-
+if __name__ == '__main__':
+    main()
